42bf0d5ff69a80fbdb423a9fa53e37df91d20025
Thomas Rooney <thomas@resiliencycollective.com>
2023-11-02 14:22:24 +0000

    chore: pipeline to make ns-npa-publisher

---
+++ terraform-provider-ns-npa-publisher/examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    npa-publisher = {
+      source  = "netskope/npa-publisher"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "npa-publisher" {
+  # Configuration options
+}
