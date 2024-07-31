c42ae5c7a4e5c0289d3d9c5a3cadff3abde906df
Justin Adrian <jadrian@netskope.com>
2024-07-31 16:02:37 -0400

    new

--- internal/sdk/internal/hooks/hookMyAppAfterSuccess.go
+++ internal/sdk/internal/hooks/hookMyAppAfterSuccess.go
@@ -6,17 +6,26 @@ import (
     "io"
     "net/http"
     "strings"
+	"log"
 )
 
-type MyAppResponse struct{}
+type MyAppResponse struct{
+	data map[string]interface{}
+}
 
 var (
     _ afterSuccessHook = (*MyAppResponse)(nil)
 )
 
 func (i *MyAppResponse) AfterSuccess(hookCtx AfterSuccessContext, res *http.Response) (*http.Response, error) {
-    if hookCtx.OperationID == "createNPAPrivateApps" || hookCtx.OperationID == "getNPAPrivateApp" {
-        var responseMap map[string]interface{}
+	log.Print(hookCtx)
+	log.Print("--------------------")
+	log.Print(res.Body)
+	if hookCtx.OperationID == "createNPAPrivateApps" || hookCtx.OperationID == "getNPAPrivateApp" {
+        log.Print(hookCtx.OperationID)
+        log.Print("inside the if statement")
+		
+		var responseMap MyAppResponse
 
         // Read and unmarshal the response body
         body, err := io.ReadAll(res.Body)
@@ -24,23 +33,29 @@ func (i *MyAppResponse) AfterSuccess(hookCtx AfterSuccessContext, res *http.Resp
             return nil, fmt.Errorf("Error reading response body: %w", err)
         }
 
-        defer res.Body.Close()
+		log.Print("--------------------")
+		log.Print(body)
+
+        // defer res.Body.Close()
 
         // Unmarshal the raw response into a map
-        if err := json.Unmarshal(body, &responseMap); err != nil {
+        if err := json.Unmarshal(body, &responseMap.data); err != nil {
             return nil, fmt.Errorf("failed to unmarshal response: %w", err)
         }
 
+		log.Print("--------------------")
+		log.Print(responseMap.data)
+
         // Modify the response as needed
-        if val, ok := responseMap["app_name"]; ok {
+        if val, ok := responseMap.data["app_name"]; ok {
             if strVal, isString := val.(string); isString {
                 cleanedValue := strings.Trim(strVal, "[]")
-                responseMap["app_name"] = cleanedValue
+                responseMap.data["app_name"] = cleanedValue
             }
         }
 
         // Marshal the modified response back to json.RawMessage
-        modifiedBody, err := json.Marshal(responseMap)
+        modifiedBody, err := json.Marshal(responseMap.data)
         if err != nil {
             return nil, fmt.Errorf("failed to marshal modified response: %w", err)
         }
@@ -50,5 +65,6 @@ func (i *MyAppResponse) AfterSuccess(hookCtx AfterSuccessContext, res *http.Resp
 
         return res, nil
     }
+	log.Print("outside the if statement")
     return res, nil
 }
